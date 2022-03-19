75f4100e4697f64fa917e254ad356ac27ff387a5
Bunyamin <bunyaminbektas2844@gmail.com>
2022-03-19 18:44:29 +0000

    coo

---
+++ src/test/java/Jekins/Test01.java
@@ -0,0 +1,13 @@
+package Jekins;
+
+import org.junit.Test;
+
+public class Test01 {
+
+    @Test
+    public void test001(){
+
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
